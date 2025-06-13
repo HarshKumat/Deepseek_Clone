ccea80e8cc0323c55dfe8331967e6c74db145306
Harsh Kumat <HarshKumat>
2025-06-13 12:58:03 +0530

    Initial Icons

--- app/page.js
+++ app/page.js
@@ -1,7 +1,23 @@
+'use client';
+import { assets } from "@/assets/assets";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+
+  const [expand,setExpand] = useState(false)
+  const [messages,setMessages] = useState([])
+  const [isLoading,setisLoading] = useState(false)
+
   return (
-    <div></div>  
+      <div className="flex h-screen">
+        {/* -- sidebar -- */}
+        <div>
+          <div>
+            <Image className="rotate-180" src={assets.menu_icon} alt=""/>
+            <Image className="opacity-70" src={assets.chat_icon} alt=""/>
+          </div>
+        </div>
+    </div>
   );
 }
